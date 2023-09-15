a326c6ed1f33a6b6dcb42de076804e927e289be9
Humza <humza718@gmail.com>
2023-09-15 19:13:04 +0000

    Sep 15, 2023, 3:13 PM

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -1,17 +1,6 @@
 function factorial(num){
-    if (num === 1) console.log(1);
+    if (num === 1) return 1;
     return num * factorial(num-1)
 }
 
 factorial(4)
-
-
-function adder(num) {
-    let total = 0;
-    for (let i = 1; i <= num; i++) {
-      total += i;
-    }
-    return total;
-  }
-
-adder(2);
