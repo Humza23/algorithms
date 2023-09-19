1deaf6c5a34e3e863c82423a152e0d112db2e655
Humza <humza718@gmail.com>
2023-09-19 11:45:43 -0400

    update

--- recursion/recursionNotes.js
+++
@@ -1,5 +0,0 @@
-// Rules
-// - base case
-// - different input 
-// - working your way towards ending the recursion
-// - return statement for base case and recursion
---
+++ recursion/reverseString.js
@@ -0,0 +1,28 @@
+function reverse(str){
+    // add whatever parameters you deem necessary - good luck!
+    let newStr = ''
+    
+    function helper(helperInput){
+      if(helperInput.length === 0) return;
+          
+      if(helperInput !== ''){
+          
+        newStr = newStr + helperInput.slice(-1)
+      }
+      helper(helperInput.slice(0,-1))
+      console.log(helperInput)
+      
+    }
+    helper(str)
+    
+    return newStr
+  }
+
+//   function reverse(str){
+// 	if(str.length <= 1) return str;
+// 	return reverse(str.slice(1)) + str[0];
+// }
+
+reverse('awesome')
+// console.log(reverse('awesome'))
+  // reverse('rithmschool') // 'loohcsmhtir'
